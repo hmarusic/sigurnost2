fc8cc31b8e3cc87ac2e2266989ede5f398919553
hmarusic <hrvojemarusic30@gmail.com>
2021-11-02 17:13:29 +0100

    treći commit

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print("Hello world")
+print("Dobrodošli u Unidu sustav!")
+print("Za prijavu upišite broj 1, za registraciju broj 2: ")
+broj = int(input("Unesite broj: "))
+
+if broj == 1:
+    print("Dobrodošli u prijavu!")
+elif broj == 2:
+    print("Dobrodošli u registraciju!")
+
+
